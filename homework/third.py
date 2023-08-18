63bf2f5fdc06efdfd7623786bd42e4195bb597d8
Sererim <danielleymanw@gmail.com>
2023-08-18 18:18:15 +0300

    Final commit. Simple class for a person. Has first name, second name, family name and age fields, three methods, for writting out full name, age and raising age by one.

--- homework/third.py
+++ homework/third.py
@@ -5,3 +5,29 @@
 #     ваш выбор.
 #     Убедитесь, что свойство возраст недоступно для прямого
 #     изменения, но есть возможность получить текущий возраст.
+
+class Person:
+    
+    def __init__(self, first_name: str, second_name: str, family_name: str, age: int) -> None:
+        self.__first_name = first_name
+        self.__second_name = second_name
+        self.__family_name = family_name
+        self.__age = age
+    
+    def full_name(self) -> str:
+        return f"{self.__family_name} {self.__first_name} {self.__second_name}"
+    
+    def birthday(self):
+        self.__age += 1
+    
+    def get_age(self) -> int:
+        return self.__age
+    
+    
+if __name__ == "__main__":
+    man = Person("Null", "Nullovich", "Nullov", 20)
+    print(man.full_name())
+    print(man.get_age())
+    print(man.birthday())
+    # print(man.__age)
+ 
