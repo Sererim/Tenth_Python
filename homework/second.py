0066ca32b692f4e21cf3e69aa29c8881ed8fa9c7
Sererim <danielleymanw@gmail.com>
2023-08-18 16:29:05 +0300

    Final commit for the rectangle class. That calculates the area and a perimeter of a rectangle and square.

--- homework/second.py
+++ homework/second.py
@@ -5,3 +5,26 @@
 #     и площадь.
 #     Если при создании экземпляра передаётся только одна
 #     сторона, считаем что у нас квадрат.
+
+class Rectangle:
+    
+    def __init__(self, lenght: float, height: float = 0) -> None:
+        self.lenght = lenght
+        self.height = height
+    
+    def perimeter(self) -> float:
+        if self.height == 0:
+            return self.lenght * 4
+        return 2 * (self.lenght + self.height)
+    
+    def area(self) -> float:
+        if self.height == 0:
+            return self.lenght * self.lenght
+        return self.lenght * self.height
+    
+
+if __name__ == "__main__":
+    abcd = Rectangle(10)
+    print(f"{abcd.perimeter() = }\n{abcd.area() = }")
+    dcba = Rectangle(10, 5)
+    print(f"{dcba.perimeter() = }\n{dcba.area() = }")
