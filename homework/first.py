0b63c6e474755f0b94d5404535ba62f86422599f
Sererim <danielleymanw@gmail.com>
2023-08-18 15:31:12 +0300

    Final commit for this class. Simple class for calculating basic parametrs of a circle. Its area and circumference.

--- homework/first.py
+++ homework/first.py
@@ -4,3 +4,21 @@
 #     У класса должно быть два метода, возвращающие длину
 #     окружности и её площадь.
 
+class Circle:
+    
+    _pi: float = 3.141592654
+    
+    def __init__(self, radius: float) -> None:
+        self.radius = radius
+    
+    def circumference(self) -> float:
+        return (2 * self._pi * self.radius).__round__(3)
+    
+    def area(self) -> float:
+        return (self._pi * (self.radius)**2).__round__(3)
+    
+
+if __name__ == "__main__":
+    round = Circle(10)
+    print(f"{round.circumference() = }")
+    print(f"{round.area() = }")
